0a02d3be63b237c004e53a7c30881d27a2103aa6
Emerson Max <emersonmax139@gmail.com>
2020-04-13 14:13:40 -0400

    Converted APIs to classes

--- api/alpaca.js
+++ api/alpaca.js
@@ -1,37 +1,33 @@
 const fetch = require('node-fetch');
-const Alpaca = require('@alpacahq/alpaca-trade-api');
 const config = require('../config/main.js')
 
 console.log("Starting Alpaca")
 
-const alpaca = new Alpaca({
-    keyId: config.alpaca.API_KEY,
-    secretKey: config.alpaca.SECRET,
-    paper: true,
-})
+class Alpaca_Api {
 
-// alpaca.getAccount().then((account) => {
-//     console.log('Current Account:', account)
-// })  
+    constructor() {
+        this.Alpaca = require('@alpacahq/alpaca-trade-api');
+        this.alpaca = new this.Alpaca({
+            keyId: config.alpaca.API_KEY,
+            secretKey: config.alpaca.SECRET,
+            paper: true,
+        })
+    }
 
-let ticker = 'QQQ';
-let price = '12';
-
-const Alpaca_Api = {
     /**
      * @param {String} ticker
      * @param {Number} qty - # of shares you wanna buy 
      *  Submit a market order at market price
      */
-    createBuyOrder: (ticker, qty) => {
-        alpaca.createOrder({
+    createBuyOrder(ticker, qty) {
+        this.alpaca.createOrder({
             symbol: ticker,
             qty: qty,
             side: 'buy',
             type: 'market',
             time_in_force: 'day'
         })
-    },
+    }
 
     /**
      * @param {String} ticker 
@@ -39,8 +35,8 @@ const Alpaca_Api = {
      * Submit a limit order to attempt to sell at
      * particular price when the market opens
      */
-    createSellOrder: (ticker, qty) => {
-        alpaca.createOrder({
+    createSellOrder(ticker, qty) {
+        this.alpaca.createOrder({
             symbol: 'AMD',
             qty: 1,
             side: 'sell',
@@ -48,6 +44,13 @@ const Alpaca_Api = {
             time_in_force: 'opg',
             limit_price: 20.50
         })
+    }
+
+    /**
+     * Get all positions for logged in account
+     */
+    getPositions() {
+        return this.alpaca.getPositions()
     }
 }
 
--- api/polygon.js
+++ api/polygon.js
@@ -3,70 +3,70 @@ const config = require('../config/main.js');
 
 let ticker = 'QQQ';
 
-const API_KEY = config.alpaca.API_KEY;
 
+class Polygon_Api {
+    constructor() {
+        this.API_KEY = config.alpaca.API_KEY;
+    }
 
-const Polygon_Api = {
+    /**
+     * @param {String} ticker
+     * 
+     */
+    getStockPriceByTicker(ticker) {
 
-    "polygon": {
-
-        /**
-         * @param {String} ticker
-         * 
-         */
-        getStockPriceByTicker: (ticker) => {
-
-            let price = 0; 
-            return new Promise((resolve, reject) => {
-                fetch(`https://api.polygon.io/v1/last/stocks/${ticker}?apiKey=${API_KEY}`, {
-                    method: "GET"
-                })
-                .then(response => response.json())
-                .then(json => {
-                    return resolve(json);
-                })
-                .catch(err => {
-                    return reject(err);
-                })
-            })    
-        },
-
-        /**
-         * Returns top 20 gaining stocks for the day (top % gain)
-         */
-        getTopGainers: () => {
-            return new Promise((resolve, reject) => {
-                fetch(`https://api.polygon.io/v2/snapshot/locale/us/markets/stocks/gainers?apiKey=${API_KEY}`, {
-                    method: "GET"
-                })
-                .then(response => response.json())
-                .then(json => {
-                    return resolve(json);
-                })
-                .catch(err => {
-                    return reject(err);
-                })
+        let price = 0; 
+        return new Promise((resolve, reject) => {
+            fetch(`https://api.polygon.io/v1/last/stocks/${ticker}?apiKey=${this.API_KEY}`, {
+                method: "GET"
             })
-        },
-        /**
-         * Returns some info on a given date range
-         * @params {String} Date 
-         */
-        getAggregateData: (ticker, multiplier, start, end) => {
-            return new Promise((resolve, reject) => {
-                fetch(`https://api.polygon.io/v2/aggs/ticker/${ticker}/range/${multiplier}/day/${start}/${end}?apiKey=${API_KEY}`, {
-                    method: "GET"
-                })
-                .then(response => response.json())
-                .then(json => {
-                    return resolve(json);
-                })
-                .catch(err => {
-                    return reject(err);
-                })
+            .then(response => response.json())
+            .then(json => {
+                return resolve(json);
             })
-        }
-    }    
+            .catch(err => {
+                return reject(err);
+            })
+        })    
+    }
+
+    /**
+     * Returns top 20 gaining stocks for the day (top % gain)
+     */
+    getTopGainers() {
+        return new Promise((resolve, reject) => {
+            fetch(`https://api.polygon.io/v2/snapshot/locale/us/markets/stocks/gainers?apiKey=${this.API_KEY}`, {
+                method: "GET"
+            })
+            .then(response => response.json())
+            .then(json => {
+                return resolve(json);
+            })
+            .catch(err => {
+                return reject(err);
+            })
+        })
+    }
+    
+    /**
+     * Returns some info on a given date range
+     * @params {String} Date 
+     */
+    getAggregateData(ticker, multiplier, start, end) {
+        return new Promise((resolve, reject) => {
+            fetch(`https://api.polygon.io/v2/aggs/ticker/${ticker}/range/${multiplier}/day/${start}/${end}?apiKey=${this.API_KEY}`, {
+                method: "GET"
+            })
+            .then(response => response.json())
+            .then(json => {
+                return resolve(json);
+            })
+            .catch(err => {
+                return reject(err);
+            })
+        })
+    }
+
 }
 
 module.exports = Polygon_Api
