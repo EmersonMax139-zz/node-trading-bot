d89321312fc7923434329137d4d5f90f96af4413
Emerson Max <emersonmax139@gmail.com>
2020-04-22 13:01:19 -0400

    Prod setup

---
+++ config/main.js
@@ -0,0 +1,10 @@
+// Make sure this is paper creds unless you wanna lose all your money
+
+const credentials = {
+    "alpaca": {
+        API_KEY: 'PKTBFVCAA0WII1UDK7H7',
+        SECRET: 'NE2lSj83kiFYFGOfZ6E3MDqGk21PWWbeqoTFWIcu'
+    }
+}
+
+module.exports = credentials;
